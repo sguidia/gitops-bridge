142a4981832da2673c56eee07b6a7fb1e7e34e47
Guidia <guidise@MB-JYMGCRWLM6.local>
2025-07-24 14:37:43 -0500

    first commit

---
+++ terraform/terraform.tfvars
@@ -0,0 +1,27 @@
+vpc_cidr = "10.0.0.0/16"
+region   = "us-east-1"
+kubernetes_version = "1.33"
+
+addons = {
+  enable_argocd                       = true
+  enable_aws_load_balancer_controller = true
+  enable_aws_ebs_csi_resources        = true
+  enable_metrics_server               = true
+}
+
+
+# Addons Git Configuration
+gitops_addons_org      = "https://github.com/sguidia/"
+gitops_addons_repo     = "gitops-bridge"
+gitops_addons_revision = "main"
+gitops_addons_basepath = ""
+gitops_addons_path     = "addons"
+
+
+# Workloads Git Configuration
+gitops_workload_org      = "https://github.com/sguidia/"
+gitops_workload_repo     = "gitops-bridge"
+gitops_workload_revision = "main"
+gitops_workload_basepath = ""
+gitops_workload_path     = "workloads"
+
---
+++ terraform/variables.tf
@@ -0,0 +1,71 @@
+variable "vpc_cidr" {
+  description = "VPC CIDR"
+  type        = string
+}
+
+variable "region" {
+  description = "AWS region"
+  type        = string
+}
+
+variable "kubernetes_version" {
+  description = "Kubernetes version"
+  type        = string
+}
+
+variable "addons" {
+  description = "Kubernetes addons"
+  type        = map(bool)
+}
+
+# Addons Git Variables
+variable "gitops_addons_org" {
+  description = "Git repository org/user contains for addons"
+  type        = string
+}
+
+variable "gitops_addons_repo" {
+  description = "Git repository contains for addons"
+  type        = string
+}
+
+variable "gitops_addons_revision" {
+  description = "Git repository revision/branch/ref for addons"
+  type        = string
+}
+
+variable "gitops_addons_basepath" {
+  description = "Git repository base path for addons"
+  type        = string
+}
+
+variable "gitops_addons_path" {
+  description = "Git repository path for addons"
+  type        = string
+}
+
+# Workloads Git Variables
+variable "gitops_workload_org" {
+  description = "Git repository org/user contains for workload"
+  type        = string
+}
+
+variable "gitops_workload_repo" {
+  description = "Git repository contains for workload"
+  type        = string
+}
+
+variable "gitops_workload_revision" {
+  description = "Git repository revision/branch/ref for workload"
+  type        = string
+}
+
+variable "gitops_workload_basepath" {
+  description = "Git repository base path for workload"
+  type        = string
+}
+
+variable "gitops_workload_path" {
+  description = "Git repository path for workload"
+  type        = string
+}
